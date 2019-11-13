33b1c87c71429d57b949196e22c840ddb86a8b09
Fellipe Sales <fellipesales@gmail.com>
2019-11-13 19:57:03 -0300

    first

---
+++ Promisse/main.js
@@ -0,0 +1,26 @@
+
+var minhaPromisse = function () {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/fellipesales');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText))
+                } else{
+                    reject('Erro narequisição')
+                }
+            }
+        }
+    })
+}
+
+minhaPromisse()
+.then(function(response){
+    console.log(response);
+})
+.catch(function(error){
+    console.warn(error);
+});
