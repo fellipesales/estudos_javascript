624c30057c23501cc018c40c0b0189798856b833
Fellipe Sales <fellipesales@gmail.com>
2019-11-21 23:19:25 -0300

    Checar Idade usando promisses com delay

---
+++ Promisses/ChecarIdade/main.js
@@ -0,0 +1,21 @@
+
+var checarIdade = function (idade) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            if (idade > 18) {
+                resolve('Maior que 18');
+            } else {
+                reject('Menor que 18');
+            }
+        }, 2000);
+    })
+}
+
+
+checarIdade(20)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.warn(error);
+    });
